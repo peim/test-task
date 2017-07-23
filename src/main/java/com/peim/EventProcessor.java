15605018dc78afb055b1dc89f1779c6195db60c2
Petrov Ivan <ivanpetrov0005@gmail.com>
2017-07-23 18:11:01 +0300

    add project

--- src/main/java/com/peim/EventProcessor.java
+++ src/main/java/com/peim/EventProcessor.java
@@ -9,8 +9,8 @@ public class EventProcessor {
     private ScheduledExecutorService executor = Executors.newScheduledThreadPool(4);
 
     public synchronized ScheduledFuture<Integer> processEvent(LocalDateTime time, Callable<Integer> task) {
-        long epochMilli = time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        long delay = epochMilli - System.currentTimeMillis();
+        long taskStartTime = time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        long delay = taskStartTime - System.currentTimeMillis();
         return executor.schedule(task, delay, TimeUnit.MILLISECONDS);
     }
 }
